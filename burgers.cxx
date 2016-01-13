b2fe751d57d07c41912bc5d401a1a5cd7e7e92d0
thisisnousername <markus.rosendahl@hhu.de>
2016-01-13 18:04:51 +0100

    superspitze

--- burgers.cxx
+++ burgers.cxx
@@ -6,22 +6,18 @@
 //---------------------------------------
 using namespace std;
 //---------------------------------------
-void writeToFile(const double* const u, const string s, const double dx,
-                 const double xmin, const int N);
-void initialize(double* const u1, double* const u0, const double dx,const double dt, const double xmin,
-                const int N);
-void step(double* const u2, const double* const u1,const double* const u0,
-          const double dt, const double dx, const int N);
+void writeToFile(const double* const u, const string s, const double dx, const double xmin, const int N);
+void initialize(double* const u1, double* const u0, const double dx,const double dt, const double xmin, const int N);
+void step(double* const u2, const double* const u1, const double* const u0, const double dt, const double dx, const int N);
 
 //---------------------------------------
 int main(){
 
   const double tEnd = 0.15 ;
 
-
-  const int N  = 64;
+  const int N  = 256;
   const double xmin = 0;
-  const double xmax = 1;
+  const double xmax = 1.0;
   const double dx = (xmax-xmin)/N ;
   double dt = 0.001;
   double t = 0;
@@ -44,7 +40,8 @@ int main(){
   {
    for(int j=0; j<Nk; j++){
 
-      // step + swap here
+      step(u2, u1, u0, dt, dx, N);
+      h=u0; u0=u1; u1=u2; u2=h;
 
       t +=dt;
    }
@@ -61,27 +58,29 @@ int main(){
   return 0;
 }
 //-----------------------------------------------
-void step(double* const u2, const double* const u1,const double* const u0,
-          const double dt, const double dx, const int N)
+void step(double* const u2, const double* const u1,const double* const u0, const double dt, const double dx, const int N)
 {
-
-
+	u2[0] = u0[0]-u1[0]*dt/dx*(u1[1]-u1[N-1]);
+	for(int i = 1; i<N-1; i++) u2[i] = u0[i]-u1[i]*dt/dx*(u1[i+1]-u1[i-1]);
+	u2[N-1] = u0[N-1]-u1[N-1]*dt/dx*(u1[0]-u1[N-2]);
 }
 //-----------------------------------------------
-void initialize(double* const u1, double* const u0, const double dx,
-                const double dt, const double xmin,  const int N)
+void initialize(double* const u1, double* const u0, const double dx, const double dt, const double xmin,  const int N)
 {
+   const double pi = acos(-1.0);
    double u,ux, uxx;
    for(int i=0; i<N; i++)
    {
      double x = xmin + i*dx;
-
-     
+     u = sin(2.0*pi*x);
+     ux = 2.0*pi*cos(2.0*pi*x);
+     uxx = 4.0*pi*pi*sin(2.0*pi*x);
+     u0[i] = u + dt*u*ux + 0.5*dt*dt*(2.0*u*ux*ux+u*uxx);
+     u1[i] = u;
    }
 }
 //-----------------------------------------------
-void writeToFile(const double* const u, const string s, const double dx,
-                 const double xmin, const int N)
+void writeToFile(const double* const u, const string s, const double dx, const double xmin, const int N)
 {
    ofstream out(s.c_str());
    for(int i=0; i<N; i++){
